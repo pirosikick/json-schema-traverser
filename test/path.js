b4cce48f788583926e1364a6aa43705999e09a4d
Hiroyuki Anai <pirosikick@gmail.com>
2016-05-01 22:29:11 +0900

    Added testcase

--- test/path.js
+++ test/path.js
@@ -1,15 +1,34 @@
 import test from 'ava';
 import NodePath from '../lib/path';
 
-test('path.visit())', t => {
+test('path.visit()', t => {
   t.plan(4);
   const schema = { type: 'object', properties: {} };
   const path = new NodePath(schema, {
-    enter(node) { t.is(node, schema); },
-    exit(node) { t.is(node, schema); },
+    enter(path) { t.is(path.node, schema); },
+    exit(path) { t.is(path.node, schema); },
     object: {
-      enter(node) { t.is(node, schema); },
-      exit(node) { t.is(node, schema); }
+      enter(path) { t.is(path.node, schema); },
+      exit(path) { t.is(path.node, schema); }
+    }
+  });
+  path.visit();
+});
+
+test('check fields of the path', t => {
+  t.plan(4);
+  const schema = {
+    type: 'object',
+    properties: {
+      hoge: { type: 'string' }
+    }
+  };
+  const path = new NodePath(schema, {
+    string(path) {
+      t.is(path.parent, schema);
+      t.is(path.container, schema.properties);
+      t.is(path.key, 'hoge');
+      t.is(path.listKey, 'properties');
     }
   });
   path.visit();
